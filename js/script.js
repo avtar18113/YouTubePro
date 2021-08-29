4ce45f7a21cf3151dd681f1c56d89f5d6301e591
avtar18113 <ramavtardhangar0@gmail.com>
2021-08-29 23:55:45 +0530

    header nav done
    youtube header navbar done with change navbar onclick

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var  menuIcon = document.querySelector(".menu-icon");
 var sideBar = document.querySelector(".sidebar");
 
-menuIcon.onClick = function(){
-    sideBar.classList.toggle(".small-sidebar");
+menuIcon.onclick = function(){
+    sideBar.classList.toggle("small-sidebar");
 }
